b8287a5076401eb16b4a8b32093d430f8bb99a93
nishu s kamal <nishusk194@gmail.com>
2021-07-18 20:27:36 +0530

    update upto 18-07-2021

---
+++ Assignment_15_07_2021_OOPS/Q2.py
@@ -0,0 +1,8 @@
+x = int(input("Enter the first number : "))
+y = int(input("Enter the second number : "))
+y = x + y;
+print("Swapping done")
+x = y - x;
+print("First number is :", x)
+y = y - x;
+print("second number is : ", y)
